c3df806f786c72d1bf74db516cbdf87e6d222baf
m5bhagat <m5bhagat@uwaterloo.ca>
2018-09-07 06:38:23 -0400

    Edited all textures for game

--- graphicaldisplay.h
+++ graphicaldisplay.h
@@ -9,30 +9,31 @@
 #include <string>
 
 const int medWidth = 800;
-const int medHeight = 600;
+const int medHeight = 500;
 const int lrgWidth = 1024;
 const int lrgHeight = 768;
 
 class GraphicalDisplay {
-    int width = medWidth;
-    int height = medHeight;
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
-    std::shared_ptr<Game> gm = nullptr;
-    std::map<std::string, SDL_Texture*> textures;
+	int width = medWidth;
+	int height = medHeight;
+	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	std::shared_ptr<Game> gm = nullptr;
+	std::map<std::string, SDL_Texture*> textures;
 
-public:
-    GraphicalDisplay(std::string size, std::shared_ptr<Game> gm);
-    bool initDisplay();
-    bool loadMedia();
-    void drawGrid();
-    void drawSuccessScreen();
-    void drawFailureScreen();
-    void drawGameOver();
-    void drawMenu();
-    void clearScreen();
-    void renderPresent();
-    void closeDisplay();
+	public:
+	GraphicalDisplay(std::string size, std::shared_ptr<Game> gm);
+	bool initDisplay();
+	bool loadMedia();
+	void drawGrid();
+	void drawSuccessScreen();
+	void drawFailureScreen();
+	void drawHelpScreen();
+	void drawGameOver();
+	void drawMenu();
+	void clearScreen();
+	void renderPresent();
+	void closeDisplay();
 };
 
 #endif
